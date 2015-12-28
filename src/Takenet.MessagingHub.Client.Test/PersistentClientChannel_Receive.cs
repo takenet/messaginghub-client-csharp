6f80d4e0f0c3f485cf9b625f55c9a3b298082517
Aldo Catella (Takenet) <aldo@takenet.com.br>
2015-12-28 13:43:07 -0200

    Created Persistent Client Channel receiver tests

--- src/Takenet.MessagingHub.Client.Test/PersistentClientChannel_Receive.cs
+++ src/Takenet.MessagingHub.Client.Test/PersistentClientChannel_Receive.cs
@@ -6,6 +6,10 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Shouldly;
+using Lime.Protocol;
+using NSubstitute;
+using Lime.Protocol.Network;
+using NSubstitute.Core;
 
 namespace Takenet.MessagingHub.Client.Test
 {
@@ -23,10 +27,34 @@ namespace Takenet.MessagingHub.Client.Test
         {
             PersistentClientChannel.StartAsync().Wait();
 
-            var message = PersistentClientChannel.ReceiveMessageAsync(CancellationToken.None).Result;
+            Message message;
 
+            using (var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(2)))
+            {
+                message = PersistentClientChannel.ReceiveMessageAsync(cancellationTokenSource.Token).Result;
+
+                cancellationTokenSource.IsCancellationRequested.ShouldNotBe(true);
+            }
+
+            message.ShouldNotBeNull();
             message.From.ShouldBe(DefaultMessage.From);
             message.To.ShouldBe(DefaultMessage.To);
+        }
+
+        [Test]
+        public void PersistentClientChannel_Receive_Message_Disconnected_Should_timeout()
+        {
+            PersistentClientChannel.StartAsync().Wait();
+
+            LimeSessionProvider.IsSessionEstablished(null).ReturnsForAnyArgs(false);
+            Transport.ReceiveAsync(CancellationToken.None).ReturnsForAnyArgs((Func<CallInfo,Envelope>)(c => { throw new LimeException(1,"Session is not estabilished"); }));
+
+            using (var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(2)))
+            {
+                PersistentClientChannel.ReceiveMessageAsync(cancellationTokenSource.Token).ShouldThrow<OperationCanceledException>();
+
+                cancellationTokenSource.IsCancellationRequested.ShouldBe(true);
+            }
         }
     }
 }
