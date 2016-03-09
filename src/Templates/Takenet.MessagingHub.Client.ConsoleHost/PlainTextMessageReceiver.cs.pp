94a5312d5d0eec80122e0548737e4f88c2efc5f9
André C. O. Minelli <andre@takenet.com.br>
2016-03-08 22:12:03 -0300

    Add ConsoleHost template nuget package

---
+++ src/Templates/Takenet.MessagingHub.Client.ConsoleHost/PlainTextMessageReceiver.cs.pp
@@ -0,0 +1,16 @@
+﻿using Lime.Protocol;
+using System;
+using System.Threading.Tasks;
+using Takenet.MessagingHub.Client.Receivers;
+
+namespace $rootnamespace$
+{
+    public class PlainTextMessageReceiver : MessageReceiverBase
+    {
+        public async override Task ReceiveAsync(Message message)
+        {
+            Console.WriteLine($"From: {message.From} \tContent: {message.Content}");
+            await EnvelopeSender.SendMessageAsync("Obrigado pela sua mensagem", message.From.Name);
+        }
+    }
+}
---
+++ src/Templates/Takenet.MessagingHub.Client.ConsoleHost/Program.cs.pp
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Takenet.MessagingHub.Client;
+
+namespace $rootnamespace$
+{
+    class Program
+    {
+        private const string Login = "<yout login>";
+        private const string AccessKey = "<yout access key>";
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var client = new MessagingHubClientBuilder()
+                .UsingAccessKey(Login, AccessKey)
+                .AddMessageReceiver(new PlainTextMessageReceiver(), forMimeType: MediaTypes.PlainText)
+                .Build();
+
+            await client.StartAsync();
+
+            Console.WriteLine("Press any key to stop");
+            Console.ReadKey();
+
+            await client.StopAsync();
+        }
+    }
+}
