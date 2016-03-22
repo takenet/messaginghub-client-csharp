ef21d27aafdb44fb07870842bf7042dd925e103b
André Bires <andrebires@gmail.com>
2016-03-22 11:29:51 -0300

    Added reminder sample

--- src/Samples/Reminder/Properties/AssemblyInfo.cs
+++ src/Samples/Reminder/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtualAssistant")]
+[assembly: AssemblyTitle("Reminder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtualAssistant")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Reminder")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7d3baa23-a118-4272-8798-b9defa18eff8")]
+[assembly: Guid("320db5d6-983a-4a8c-812d-c60734793625")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ src/Samples/Reminder/ReminderProcessor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.Textc;
+
+namespace Reminder
+{
+    public class ReminderProcessor
+    {
+        private readonly List<Reminder> _reminders;
+
+        public ReminderProcessor()
+        {
+            _reminders = new List<Reminder>();
+        }
+
+        public Task<string> AddReminderAsync(string text, string when, IRequestContext context)
+        {
+            var whenDateTime = ParseWhen(when);
+
+            var reminder = new Reminder(text, whenDateTime);
+            _reminders.Add(reminder);
+            context.RemoveVariable(nameof(when));
+            return $"Lembrete '{reminder.Text}' adicionado com sucesso para '{reminder.When}'".AsCompletedTask();
+        }
+
+        public Task<string> GetRemindersAsync(string when)
+        {
+            var reminders = string.IsNullOrEmpty(when) ? 
+                _reminders : 
+                _reminders.Where(r => r.When.Date.Equals(ParseWhen(when)));
+
+            if (!reminders.Any())
+            {
+                return !string.IsNullOrWhiteSpace(when) ? 
+                    $"Não há lembretes definidos para {when}".AsCompletedTask() : 
+                    "Não há lembretes definidos".AsCompletedTask();
+            }
+
+            var remindersDictionary = reminders
+                .GroupBy(r => r.When)
+                .ToDictionary(r => r.Key, r => r.Select(reminder => reminder.Text));
+
+            var messageBuilder = new StringBuilder();
+
+            foreach (var date in remindersDictionary.Keys)
+            {
+                messageBuilder.AppendLine($"Lembretes para '{date}':");
+                foreach (var reminderMessage in remindersDictionary[date])
+                {
+                    messageBuilder.AppendLine($"* {reminderMessage}");
+                }
+
+                messageBuilder.AppendLine();
+            }
+
+            return Task.FromResult(messageBuilder.ToString());
+        }
+
+        public class Reminder
+        {
+            public Reminder(string text, DateTime when)
+            {
+                Text = text;
+                When = when;
+            }
+
+            public string Text { get; }
+
+            public DateTime When { get; }
+        }
+
+        public static DateTime ParseWhen(string when)
+        {
+            switch (when)
+            {
+                case "yesterday":
+                case "ontem":
+                    return DateTimeOffset.UtcNow.AddDays(-1).Date;
+
+                case "today":
+                case "hoje":
+                    return DateTimeOffset.UtcNow.Date;
+
+                case "tomorrow":
+                case "amanha":
+                case "amanhã":
+                    return DateTimeOffset.UtcNow.AddDays(1).Date;
+
+                default:
+                    return DateTime.MaxValue;
+
+            }
+        }
+    }
+}
