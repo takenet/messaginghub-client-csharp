fa00c49b0e49f6cdb5b596361c8039fe8a980aeb
Breno <brenof@takenet.com.br>
2016-10-31 15:55:56 -0200

    fix event track name

--- src/Takenet.MessagingHub.Client/Extensions/EventTrack/EventTrackExtension.cs
+++ src/Takenet.MessagingHub.Client/Extensions/EventTrack/EventTrackExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Lime.Protocol;
+using Lime.Protocol.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +13,21 @@ namespace Takenet.MessagingHub.Client.Extensions.EventTracker
 {
     public class EventTrackExtension : ExtensionBase, IEventTrackExtension
     {
-        const string EVENTRACKER_URI = "/event-track";
+        const string EVENTRACK_URI = "/event-track";
 
 
         public EventTrackExtension(IMessagingHubSender sender)
             : base(sender)
         {
+            TypeUtil.RegisterDocument<EventTrack>();
         }
 
         public async Task<IEnumerable<EventTrack>> GetAsync(DateTime filterDate, int take = 20, CancellationToken cancellationToken = new CancellationToken())
         {
-            var requestCommand = new Command()
+            var requestCommand = new Command
             {
                 Method = CommandMethod.Get,
-                Uri = new LimeUri($"{EVENTRACKER_URI}?filterDate={filterDate.ToString("yyyy-MM-dd")}&take={take}"),
+                Uri = new LimeUri($"{EVENTRACK_URI}?filterDate={filterDate.ToString("yyyy-MM-dd")}&take={take}"),
             };
 
             var response = await ProcessCommandAsync<DocumentCollection>(requestCommand, cancellationToken);
@@ -41,7 +43,7 @@ namespace Takenet.MessagingHub.Client.Extensions.EventTracker
             var requestCommand = new Command
             {
                 Method = CommandMethod.Set,
-                Uri = new LimeUri(EVENTRACKER_URI),
+                Uri = new LimeUri(EVENTRACK_URI),
                 Resource = new EventTrack
                 {
                     Action = actionName,
