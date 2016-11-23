d8d8233bdc0175b4a7da897514a28037db3b3227
André Bires Fonseca (Takenet) <andreb@takenet.com.br>
2016-11-23 17:42:34 -0200

    Added script to change output action

---
+++ src/Takenet.MessagingHub.Client.WebTemplate/install.ps1
@@ -0,0 +1,10 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function SetCopyToOutputDirectory
+{
+	$item = $project.ProjectItems.Item("application.json")
+	$item.Properties.Item("CopyToOutputDirectory").Value = 2
+}
+
+Write-Host "Setting copy to output directory..."
+SetCopyToOutputDirectory
