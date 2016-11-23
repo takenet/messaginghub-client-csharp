60f2cd1db460497ee536f00ed401e3be418e9a23
André Bires Fonseca (Takenet) <andreb@takenet.com.br>
2016-11-23 17:26:00 -0200

    Added WebTemplate package

---
+++ src/Takenet.MessagingHub.Client.WebTemplate/OwinStartup.cs.pp
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Owin;
+using Microsoft.Owin.BuilderProperties;
+using Owin;
+using Takenet.MessagingHub.Client.Host;
+
+[assembly: OwinStartup(typeof($rootnamespace$.OwinStartup))]
+
+namespace $rootnamespace$
+{
+    public class OwinStartup
+    {
+        /// <summary>
+        /// OWIN configuration. 
+        /// DO NOT CHANGE THIS METHOD!
+        /// </summary>
+        /// <param name="app">The application.</param>
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(async context =>
+            {
+                Trace.TraceInformation("Starting application...");
+
+                var applicationPath = Path.Combine(
+                    AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                    "application.json");
+                var binPath = $"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}bin";
+                var application = Application.ParseFromJsonFile(applicationPath);
+                var stopabble = await Bootstrapper.StartAsync(application, true, binPath);
+                var properties = new AppProperties(app.Properties);
+                properties.OnAppDisposing.Register(() =>
+                {
+                    Trace.TraceInformation("Stopping application...");
+                    stopabble.StopAsync().Wait();
+                    Trace.TraceInformation("Application stopped.");
+                });
+                Trace.TraceInformation("Application started.");
+            });
+        }
+    }
+}
---
+++ src/Takenet.MessagingHub.Client.WebTemplate/PlainTextMessageReceiver.cs.pp
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.MessagingHub.Client;
+using Takenet.MessagingHub.Client.Listener;
+using Takenet.MessagingHub.Client.Sender;
+using System.Diagnostics;
+
+namespace $rootnamespace$
+{
+    public class PlainTextMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+
+        public PlainTextMessageReceiver(IMessagingHubSender sender)
+        {
+            _sender = sender;
+        }
+
+        public async Task ReceiveAsync(Message message, CancellationToken cancellationToken)
+        {
+            Trace.TraceInformation($"From: {message.From} \tContent: {message.Content}");
+            await _sender.SendMessageAsync("Pong!", message.From, cancellationToken);
+        }
+    }
+}
