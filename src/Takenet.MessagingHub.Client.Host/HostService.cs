cc04c309239052504bd54467d6ac96d16e97eb5c
André Bires Fonseca (Takenet) <andreb@takenet.com.br>
2016-11-24 11:19:55 -0200

    Mhh now support installing itself as a service

---
+++ src/Takenet.MessagingHub.Client.Host/HostService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Takenet.MessagingHub.Client.Listener;
+using Topshelf;
+
+namespace Takenet.MessagingHub.Client.Host
+{
+    public class HostService : ServiceControl
+    {
+        private IStoppable _stoppable;
+
+        
+        public bool Start(HostControl hostControl)
+        {            
+            var applicationFileName = Program.GetApplicationFileName(new string[0]);
+            if (!File.Exists(applicationFileName))
+            {
+                throw new Exception($"Could not find the {applicationFileName} file");                
+            }
+
+            _stoppable = Program.StartAsync(applicationFileName).Result;
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30)))
+            {
+                _stoppable.StopAsync(cts.Token).Wait();
+            }
+            return true;
+        }
+    }
+}
--- src/Takenet.MessagingHub.Client.Host/Program.cs
+++ src/Takenet.MessagingHub.Client.Host/Program.cs
@@ -3,7 +3,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.ServiceProcess;
 using System.Threading.Tasks;
+using Takenet.MessagingHub.Client.Listener;
+using Topshelf;
 
 namespace Takenet.MessagingHub.Client.Host
 {
@@ -25,10 +28,11 @@ namespace Takenet.MessagingHub.Client.Host
                 {
                     WriteLine("Messaging Hub client host");
                     WriteLine();
-                    WriteLine("Usage: mhh <path> [-help] [-pause]");
+                    WriteLine("Usage: mhh <path> [-help] [-pause] [-service]");
                     WriteLine();
                     WriteLine("- path: The path of the application host JSON file");
                     WriteLine("- help: Show this help");
+                    WriteLine("- service: Run as a Windows Service");
                     WriteLine(
                         "- pause: Indicates if the application should pause and wait for user input after the execution");
                     WriteLine();
@@ -40,27 +44,51 @@ namespace Takenet.MessagingHub.Client.Host
                     Trace.Listeners.Add(new ConsoleTraceListener());
                 }
 
-                var applicationFileName =
-                    args.FirstOrDefault(a => !a.StartsWith("-") && !a.StartsWith("/")) ??
-                    Bootstrapper.DefaultApplicationFileName;
-
-                if (!File.Exists(applicationFileName))
+                if (HasArgument(args, "serviceName") ||
+                    HasArgument(args, "displayName") ||
+                    HasArgument(args, "serviceName"))
                 {
-                    WriteLine($"Could not find the {applicationFileName} file", ConsoleColor.Red);
-                    return;
+                    var serviceName = GetArgumentValue(args, "serviceName");
+                    if (serviceName == null)
+                    {
+                        WriteLine("The 'name' argument is mandatory", ConsoleColor.Red);
+                        return;
+                    }
+                    var serviceDisplayName = GetArgumentValue(args, "displayName") ?? "";
+                    var serviceDescription = GetArgumentValue(args, "description") ?? "";
+
+                    var commandLine = string.Empty;
+                    if (HasArgument(args, "install"))
+                    {
+                        commandLine = "install";
+                    }
+                    else if (HasArgument(args, "uninstall"))
+                    {
+                        commandLine = "uninstall";
+                    }
+
+                    HostFactory.Run(configurator =>
+                    {
+                        configurator.ApplyCommandLine(commandLine);
+                        configurator.SetServiceName(serviceName); //cannot contain spaces or / or \
+                            configurator.SetDisplayName(serviceDisplayName);
+                        configurator.SetDescription(serviceDescription);
+                        configurator.StartAutomatically();
+                        configurator.Service<HostService>();
+                    });
+                }
+                else
+                {
+                    var applicationFileName = GetApplicationFileName(args);
+                    if (!File.Exists(applicationFileName))
+                    {
+                        WriteLine($"Could not find the {applicationFileName} file", ConsoleColor.Red);
+                        return;
+                    }
+
+                    await Run(applicationFileName);
                 }
 
-                const ConsoleColor highlightColor = ConsoleColor.DarkCyan;
-
-                WriteLine("Starting application...", highlightColor);
-                ConfigureWorkingDirectory(applicationFileName);
-                var application = Application.ParseFromJsonFile(applicationFileName);
-                var stopabble = await Bootstrapper.StartAsync(application);
-                WriteLine("Application started. Press any key to stop.", highlightColor);
-                Console.Read();
-                WriteLine("Stopping application...", highlightColor);
-                await stopabble.StopAsync();
-                WriteLine("Application stopped.", highlightColor);
             }
             catch (Exception ex)
             {
@@ -74,12 +102,59 @@ namespace Takenet.MessagingHub.Client.Host
                     WriteLine("Press any key to exit.");
                     Console.ReadKey(true);
                 }
-            }            
+            }
+
+        }
+
+        private static async Task Run(string applicationFileName)
+        {
+            const ConsoleColor highlightColor = ConsoleColor.DarkCyan;
+
+            WriteLine("Starting application...", highlightColor);
+
+            var stopabble = await StartAsync(applicationFileName);
+
+            WriteLine("Application started. Press any key to stop.", highlightColor);
+            Console.Read();
+            WriteLine("Stopping application...", highlightColor);
+            await stopabble.StopAsync();
+            WriteLine("Application stopped.", highlightColor);
+        }
+
+
+        public static async Task<IStoppable> StartAsync(string applicationFileName)
+        {
+            ConfigureWorkingDirectory(applicationFileName);
+            var application = Application.ParseFromJsonFile(applicationFileName);
+            var stopabble = await Bootstrapper.StartAsync(application);
+            return stopabble;
+        }
+
+        public static string GetApplicationFileName(string[] args)
+        {
+            var applicationFileName =
+                args.FirstOrDefault(a => !a.StartsWith("-") && !a.StartsWith("/")) ??
+                Bootstrapper.DefaultApplicationFileName;
+            return applicationFileName;
         }
 
         private static bool HasArgument(string[] args, string argumentName)
         {
             return args.Any(a => a.TrimStart('-', '/').Equals(argumentName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private static string GetArgumentValue(string[] args, string argumentName)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].TrimStart('-', '/').Equals(argumentName, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (args.Length >= i + 1) return args[i + 1];
+                    return null;
+                }
+            }
+
+            return null;
         }
 
         private static void ConfigureWorkingDirectory(string applicationFileName)
