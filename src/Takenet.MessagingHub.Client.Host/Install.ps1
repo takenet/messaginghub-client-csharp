216b592d9b02adf5bc763c2a2a3939ace1e8728a
André C. O. Minelli <andre@takenet.com.br>
2016-11-28 18:17:29 -0200

    Host package updates StartProgram configuration from OutputPath (instead of tools' package folder)

--- src/Takenet.MessagingHub.Client.Host/Install.ps1
+++ src/Takenet.MessagingHub.Client.Host/Install.ps1
@@ -1,13 +1,40 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
+function Add-StartProgramIfNeeded {
+	[xml] $prjXml = Get-Content $project.FullName
+	foreach($PropertyGroup in $prjXml.project.ChildNodes)
+	{
+		if($PropertyGroup.StartAction -ne $null)
+		{
+			return
+		}
+	}
+
+	$propertyGroupElement = $prjXml.CreateElement("PropertyGroup", $prjXml.Project.GetAttribute("xmlns"));
+	$startActionElement = $prjXml.CreateElement("StartAction", $prjXml.Project.GetAttribute("xmlns"));
+	$propertyGroupElement.AppendChild($startActionElement) | Out-Null
+	$propertyGroupElement.StartAction = "Program"
+	$startProgramElement = $prjXml.CreateElement("StartProgram", $prjXml.Project.GetAttribute("xmlns"));
+	$propertyGroupElement.AppendChild($startProgramElement) | Out-Null
+	$propertyGroupElement.StartProgram = "`$(ProjectDir)`$(OutputPath)mhh.exe"
+	$prjXml.project.AppendChild($propertyGroupElement) | Out-Null
+	$writerSettings = new-object System.Xml.XmlWriterSettings
+	$writerSettings.OmitXmlDeclaration = $false
+	$writerSettings.NewLineOnAttributes = $false
+	$writerSettings.Indent = $true
+	$projectFilePath = Resolve-Path -Path $project.FullName
+	$writer = [System.Xml.XmlWriter]::Create($projectFilePath, $writerSettings)
+	$prjXml.WriteTo($writer)
+	$writer.Flush()
+	$writer.Close()
+}
+
 write-host "Trying to update mhh.exe project reference (if one exists)"
 
 $hostFile = "mhh.exe"
 $hostPath = Join-Path (Join-Path $installPath "tools\net461\") $hostFile
 
-$projectUri = [uri]$project.FullName;
-$hosttUri = [uri]$hostPath;
-$hostRelativePath = $projectUri.MakeRelative($hosttUri) -replace "/","\"
+$projectPathUri = [String](Split-Path -Path $project.FullName);
 
 $project.ProjectItems | ForEach-Object {
 	if ($_.FileNames(0).EndsWith($hostFile)) { 
@@ -19,3 +46,10 @@ $project.ProjectItems | ForEach-Object {
 		continue
 	}
 }
+
+$project.Save()
+
+Write-Host "Modifying Configurations..."
+Add-StartProgramIfNeeded
+
+
