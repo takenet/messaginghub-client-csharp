bf5625ba22bc692460d05d73603452b3afebcac0
André C. O. Minelli <andre@takenet.com.br>
2016-11-29 09:14:49 -0200

    Fix textc Nuget packaging

---
+++ src/Samples/RssContact/ForwardToOperatorChannelModule.cs
@@ -0,0 +1,45 @@
+﻿using Lime.Protocol;
+using Lime.Protocol.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Takenet.MessagingHub.Client.Sender;
+
+namespace RssContact
+{
+    public class ForwardToOperatorChannelModule : IChannelModule<Message>, IChannelModule<Command>
+    {
+        private readonly IMessagingHubSender _sender;
+
+        public ForwardToOperatorChannelModule(IMessagingHubSender sender)
+        {
+            _sender = sender;
+        }
+
+        public void OnStateChanged(SessionState state)
+        { }
+
+        public Task<Message> OnReceivingAsync(Message envelope, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Command> OnReceivingAsync(Command envelope, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Command> OnSendingAsync(Command envelope, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(envelope);
+        }
+
+        public Task<Message> OnSendingAsync(Message envelope, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(envelope);
+        }
+    }
+}
