671340fdfc27375e19deed26b4d55065c90d6aa5
André C. O. Minelli <andre@takenet.com.br>
2016-12-07 10:11:29 -0200

    Do not send $take filter on command Uri when get thread messages

--- src/Takenet.MessagingHub.Client/Extensions/Threads/ThreadExtension.cs
+++ src/Takenet.MessagingHub.Client/Extensions/Threads/ThreadExtension.cs
@@ -21,14 +21,19 @@ namespace Takenet.MessagingHub.Client.Extensions.Threads
             return ProcessCommandAsync<DocumentCollection>(requestCommand, cancellationToken);
         }
 
-        public Task<DocumentCollection> GetThreadAsync(Identity identity, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return GetThreadAsync(identity, 20, cancellationToken);
-        }
+        public Task<DocumentCollection> GetThreadAsync(Identity identity, CancellationToken cancellationToken = default(CancellationToken)) 
+            => GetThreadInternalAsync(identity, null, cancellationToken);
 
-        public Task<DocumentCollection> GetThreadAsync(Identity identity, int take, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<DocumentCollection> GetThreadAsync(Identity identity, int take, CancellationToken cancellationToken = default(CancellationToken)) 
+            => GetThreadInternalAsync(identity, take, cancellationToken);
+
+        private Task<DocumentCollection> GetThreadInternalAsync(Identity identity, int? take, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var requestUri = $"{THREADS_URI}/{identity}?$take={take}";
+            var requestUri = $"{THREADS_URI}/{identity}";
+            if (take.HasValue)
+            {
+                requestUri = $"{requestUri}?$take={take}";
+            }
             var requestCommand = CreateGetCommandRequest(requestUri);
             return ProcessCommandAsync<DocumentCollection>(requestCommand, cancellationToken);
         }
