430a0907573e9389eaa80ea55cc89771125b64bb
aldo <aldo@PEARLJAM>
2016-12-27 14:54:38 -0200

    Add more tests for command receiver #37685

--- src/Takenet.MessagingHub.Client.Test/MessagingHubClientTests_AddCommandReceiver.cs
+++ src/Takenet.MessagingHub.Client.Test/MessagingHubClientTests_AddCommandReceiver.cs
@@ -45,7 +45,54 @@ namespace Takenet.MessagingHub.Client.Test
             await Task.Delay(TIME_OUT);
 
             //Assert
+            Assert.AreEqual(CommandStatus.Pending, _someCommand.Status);
             _commandReceiver.ReceivedWithAnyArgs().ReceiveAsync(null, CancellationToken.None);
+        }
+
+        [Test]
+        public async Task Add_CommandReceiver_And_Send_Command_Response_Should_Not_Process()
+        {
+            //Arrange
+            MessagingHubListener.AddCommandReceiver(_commandReceiver);
+            await MessagingHubConnection.ConnectAsync();
+            await MessagingHubListener.StartAsync();
+
+            //Act
+            CommandProducer.Add(new Command
+            {
+                Id = EnvelopeId.NewId(),
+                Method = CommandMethod.Get,
+                Resource = new Contact(),
+                Uri = new LimeUri("lime://test/command"),
+                Status = CommandStatus.Success
+            });
+            await Task.Delay(TIME_OUT);
+
+            //Assert
+            _commandReceiver.DidNotReceiveWithAnyArgs().ReceiveAsync(null, CancellationToken.None);
+        }
+
+        [Test]
+        public async Task Add_CommandReceiver_And_Send_Command_ResponseFailure_Should_Not_Process()
+        {
+            //Arrange
+            MessagingHubListener.AddCommandReceiver(_commandReceiver);
+            await MessagingHubConnection.ConnectAsync();
+            await MessagingHubListener.StartAsync();
+
+            //Act
+            CommandProducer.Add(new Command
+            {
+                Id = EnvelopeId.NewId(),
+                Method = CommandMethod.Get,
+                Resource = new Contact(),
+                Uri = new LimeUri("lime://test/command"),
+                Status = CommandStatus.Failure
+            });
+            await Task.Delay(TIME_OUT);
+
+            //Assert
+            _commandReceiver.DidNotReceiveWithAnyArgs().ReceiveAsync(null, CancellationToken.None);
         }
 
         [Test]
